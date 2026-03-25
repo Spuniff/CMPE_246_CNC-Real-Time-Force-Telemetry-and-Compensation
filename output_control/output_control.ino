703f332dc1e45612b19be4fc1e04d7e24a26a2ed
havishmoturi23 <havishmoturi23@gmail.com>
2026-03-25 13:02:44 -0700

    Add files via upload

---
+++ output_control/output_control.ino
@@ -0,0 +1,15 @@
+const int outputPin = 25;
+
+void setup() {
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, HIGH);
+}
+
+void setOutputLow() {
+  digitalWrite(outputPin, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
